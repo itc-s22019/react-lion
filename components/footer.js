c2580d4c3eeeb398fb028e35a9f2a5e4e237a123
itc-s22019 <s22019@std.it-college.ac.jp>
2023-02-16 08:11:25 +0000

    6.5

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,17 @@
-export default function Frooter () {
-  return <footer>FOOTER</footer>
+import Container from 'components/container'
+import Logo from 'components/logo'
+import Social from 'components/social'
+import styles from 'styles/footer.module.css'
+
+export default function Footer () {
+  return (
+    <footer className={styles.wrapper}>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo />
+          <Social />
+        </div>
+      </Container>
+    </footer>
+  )
 }
